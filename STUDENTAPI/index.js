8c994e0429ab083ac9c3c570c4bc830ba6a14923
Akbaraamir <bscs2312355@szabist.pk>
2025-05-06 12:40:23 +0000

    message

--- STUDENTAPI/index.js
+++ STUDENTAPI/index.js
@@ -21,6 +21,55 @@ app.get('/gettotalstd',async(req,res)=>{
         res.status(500).json({Error:err.message});
     }
 });
+
+app.get('/employee',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(employee_id) from employees');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/jobs',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(job_id) from jobs');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/countries',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(country_id) from countries');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/regions',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(region_id) from regions');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/jobhistory',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(job_id) from job_history');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/departments',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(department_id) from departments');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
 const PORT = process.env.PORT;
 app.listen(PORT,() =>{
     console.log(`Connected Successfully...Running on PORT ${PORT}`)
